68037739a82a2b79c46a321c7ece865376acd20f
ahmedjsofficial <javascriptstack0011@gmail.com>
2025-04-05 21:13:37 +0500

    initial-commit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,50 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+          ],
+        },
+      },
+      jwtSecret: process.env.JWT_SECRET,
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:5173"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
+  {
+    name: "strapi::body",
+    config: {
+      patchKoa: true,
+      multipart: true,
+      includeUnparsed: true,
+    },
+  },
 ];
